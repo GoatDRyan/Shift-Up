6ca8531d4d10db50800523f3183e7390b0eecfe6
goatdryan <mwanzaryan01@gmail.com>
2026-03-10 20:51:35 +0100

    login modif

--- login.php
+++ login.php
@@ -11,8 +11,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (!empty($email) && !empty($password)) {
         // 1. On cherche l'utilisateur par son email
-        $stmt = $pdo->prepare("SELECT * FROM users WHERE mail = :mail");
-        $stmt->execute(['mail' => $email]);
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+        $stmt->execute(['email' => $email]);
         $user = $stmt->fetch();
 
         // 2. Vérification du mot de passe
